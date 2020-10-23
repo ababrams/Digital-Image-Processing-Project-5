895880bfbae9fb4bb17c47fddd936c90ce7ffbba
lipkinc18 <lipkinc18@students.ecu.edu>
2020-10-23 09:27:41 -0400

    Main class initialized

---
+++ LomographicFilters/src/Main.java
@@ -0,0 +1,11 @@
+package src;
+
+public class Main {
+	public static void main(String args[]) {
+		System.out.println("Welcome to the project!");
+
+		if (args.length == 0) {
+			System.out.println(" -h for information on how to use solution.");
+		}
+	}
+}
