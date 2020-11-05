573080b43b8ebd47cf1594f5a7b3db70b6192571
acler <acler@host.docker.internal>
2020-11-05 14:58:36 -0500

    updated GUI

--- LomographicFilters/src/Lomo.java
+++ LomographicFilters/src/Lomo.java
@@ -1,4 +1,5 @@
 package src;
+
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -41,32 +42,28 @@ import javax.swing.event.ChangeListener;
  *
  */
 
-public class Lomo {
-	public static Mat img;
-    private static final int ALPHA_SLIDER_MAX = 100;
-    private static final int BETA_SLIDER_MAX = 100;
-    private static int alphaVal = 0;
-    private Mat matImgSrc1;
-    private Mat matImgSrc2;
-    private Mat matImgDst = new Mat();
-    private static JFrame frame;
-    private static JLabel imgLabel;
-    private  JButton saveButton;
-    private static JSlider sliderS;
-    private static JSlider sliderR;
-    private static JPanel sliderPanel;
-    private static double r;
-    private static double s;
+public class Lomo implements ActionListener, ChangeListener {
+	public Mat img;
+	private final int ALPHA_SLIDER_MAX = 100;
+	private final int BETA_SLIDER_MAX = 100;
+	private int alphaVal = 0;
+	private JFrame frame;
+	private JLabel imgLabel;
+	private JButton saveButton;
+	private JSlider sliderS;
+	private JSlider sliderR;
+	private JPanel sliderPanel;
+	private double r;
+	private double s;
 
-    
-    
 	public static void main(String args[]) {
+		Lomo lomo = new Lomo();
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
 		if (args.length == 0) {
 			System.out.println("-h for information on how to use solution.");
 		} else {
-			commandLineParser(args[0]);
+			lomo.commandLineParser(args[0]);
 		}
 //		img = Imgcodecs.imread("images/costume.png", 1);
 //		double s = 0.20;
@@ -87,7 +84,7 @@ public class Lomo {
 	 * @param s    value input by the user that is passed into the calculation
 	 * @return
 	 */
-	public static Mat redFilter(Mat img, double s) {
+	public Mat redFilter(Mat img, double s) {
 		// makes the lookup table for the red channel
 		Mat redLUT = makeLUT(s);
 		// splits the image Mat into the 3 color channels
@@ -108,16 +105,16 @@ public class Lomo {
 	 * @param s, value the red channel is manipulated
 	 * @return lookup table matrix
 	 */
-	public static Mat makeLUT(double s) {
+	public Mat makeLUT(double s) {
 		// creates the lookup table
 		Mat lookupTable = new Mat(1, 256, CvType.CV_8UC1);
-		
-		for(double i = 0; i < 256; i++) {
+
+		for (double i = 0; i < 256; i++) {
 			double a = -(((i / 256.0) - 0.5) / s);
 			double e = Math.exp(a);
 			double result = 256.0 / (1.0 + e);
 			// sets the data for that index as the calculated data
-			lookupTable.put(0,(int) i, result);
+			lookupTable.put(0, (int) i, result);
 		}
 		return lookupTable;
 
@@ -129,7 +126,7 @@ public class Lomo {
 	 * 
 	 * @param s argument at index 0
 	 */
-	public static void commandLineParser(String f) {
+	public void commandLineParser(String f) {
 		switch (f) {
 		case "-h":
 			System.out.println("How to use this program.\n\n"
@@ -151,7 +148,7 @@ public class Lomo {
 						GUI(img);
 						System.out.println("Image: " + file.toString());
 					}
-				} catch (IOException e) {
+				} catch (Exception e) {
 					System.out.println("File could not be opened.");
 				}
 			} else if (file.isDirectory()) {
@@ -167,7 +164,7 @@ public class Lomo {
 	 * 
 	 * @param img current displayed image
 	 */
-	public static void save(Mat img) {
+	public void save(Mat img) {
 		Scanner in = new Scanner(System.in);
 
 		System.out.println("Enter filepath and filename to save current copy of image: ");
@@ -186,144 +183,144 @@ public class Lomo {
 	 * @param r   radius tracked by slider, must range 1 <= r <= 100
 	 * @return image with halo filter
 	 */
-	public static Mat haloFilter(Mat img, double r) {
-		//rMax = maximum radius.
+	public Mat haloFilter(Mat img, double r) {
+		// rMax = maximum radius.
 		int rMax = 0;
-		if (img.rows()<img.cols()) {
-			 rMax = img.rows();
+		if (img.rows() < img.cols()) {
+			rMax = img.rows();
 		} else {
 			rMax = img.cols();
 		}
-		
+
 		double radius = (r * .01) * rMax;
-		
+
 		// create new image of the same size
 		Mat mask = new Mat(img.rows(), img.cols(), CvType.CV_32FC3);
 		// assign the value of 0.75 to each element in matrix
 		for (int i = 0; i < img.rows(); i++) {
 			for (int j = 0; j < img.cols(); j++) {
-				double [] maskChannel = mask.get(i,j);
-				//red channel
+				double[] maskChannel = mask.get(i, j);
+				// red channel
 				maskChannel[0] = 0.75;
-				//green channel
+				// green channel
 				maskChannel[1] = 0.75;
-				//blue channel
+				// blue channel
 				maskChannel[2] = 0.75;
 			}
 		}
-		//finds the center point of the image
-		int centerCol= img.cols()/2;
-		int centerRow = img.rows()/2;		
-		//creates a circle on the mask with white color represented in the scalar object. 
-		Imgproc.circle(mask, new Point(centerRow, centerCol),(int) radius, new Scalar(1, 1, 1), -1);
+		// finds the center point of the image
+		int centerCol = img.cols() / 2;
+		int centerRow = img.rows() / 2;
+		// creates a circle on the mask with white color represented in the scalar
+		// object.
+		Imgproc.circle(mask, new Point(centerRow, centerCol), (int) radius, new Scalar(1, 1, 1), -1);
 
 		Size circle = new Size(radius, radius);
-		//blur the mask 
+		// blur the mask
 		Imgproc.blur(mask, mask, circle);
 		img.convertTo(img, CvType.CV_32FC3);
-		//multiply the mask image with the image manipulated by the red filter
+		// multiply the mask image with the image manipulated by the red filter
 		Mat newMat = new Mat();
 		newMat = img.mul(mask);
 		newMat.convertTo(newMat, CvType.CV_8UC3);
 		return newMat;
 	}
 
-	public static void GUI(Mat matImg) {
-		 // Create and set up the window.
-        frame = new JFrame("Linear Blend");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //Change original image to buffered image to display
-        Image image = HighGui.toBufferedImage(matImg);
-        // Set up the content pane.'
-       addComponentsToPane(frame.getContentPane(), matImg);
-        
-        Mat red = redFilter(matImg, s);
-        Mat filteredImage = haloFilter(red, r);
-		//Imgproc.resize(img, img, new Size(img.rows() / 2, img.rows() / 2), 0, 0, Imgproc.INTER_AREA);
-        Image image = HighGui.toBufferedImage(filteredImage);
-        
-     // Set up the content pane.'
-        addComponentsToPane(frame.getContentPane(), image);
-         
-        //TODO make sure image changes as s and r change
-        
-        
-        // Use the content pane's default BorderLayout. No need for
-        // setLayout(new BorderLayout());
-        // Display the window
-        frame.pack();
-        frame.setVisible(true);
-	}
-	
+	public void GUI(Mat matImg) {
+		// Create and set up the window.
+		frame = new JFrame("Linear Blend");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// Change original image to buffered image to display
+		Image image = HighGui.toBufferedImage(matImg);
+		// Set up the content pane.'
+		addComponentsToPane(frame.getContentPane(), image);
 
-	private static void addComponentsToPane(Container pane, Image image) {
-	        if (!(pane.getLayout() instanceof BorderLayout)) {
-	            pane.add(new JLabel("Container doesn't use BorderLayout!"));
-	            return;
-	        }
-	        sliderPanel = new JPanel();
-	        saveButton = new JButton("Save");
-	        imgLabel = new JLabel(new ImageIcon());
-	        sliderS = new JSlider(0, BETA_SLIDER_MAX, 0);
-	        sliderR = new JSlider(0, ALPHA_SLIDER_MAX, 0);
-	        
-	        sliderPanel.setLayout(new BoxLayout(sliderPanel, BoxLayout.PAGE_AXIS));
-	     	        
-	        sliderR.setMajorTickSpacing(20);
-	        sliderR.setMinorTickSpacing(5);
-	        sliderR.setPaintTicks(true);
-	        sliderR.setPaintLabels(true);
-	        
-	        sliderS.setMajorTickSpacing(20);
-	        sliderS.setMinorTickSpacing(5);
-	        sliderS.setPaintTicks(true);
-	        sliderS.setPaintLabels(true);
-	        
-	       // saveButton.setFocusable(true);
-	        saveButton.addActionListener( this);
-	        
-	        sliderS.addChangeListener(this); 
-	        sliderR.addChangeListener(this);
-	      
-	        sliderPanel.add(new JLabel(String.format("Alpha x %d", ALPHA_SLIDER_MAX)));        
-	        sliderPanel.add(new JLabel(String.format("Beta x %d", BETA_SLIDER_MAX)));
-	        sliderPanel.add(sliderS);
-	        sliderPanel.add(sliderR);
-	        pane.add(sliderPanel, BorderLayout.PAGE_START);
-	        pane.add(imgLabel, BorderLayout.CENTER);
-	    }
-	
-			/**
-			 * 
-			 * @param e
-			 */
-			public void actionPerformed (ActionEvent e) {
-			save()
-			}
-				
-			  /**
-	         * 
-	         * @param e
-	         */
-            public void stateChanged(ChangeEvent e) {
-            	if(e.getSource().equals(sliderS)) {
-                JSlider source = (JSlider) e.getSource();
-                r = source.getValue();
-             // update();
-            	}
-            	if(e.getSource().equals(sliderR)) {
-                JSlider source = (JSlider) e.getSource();
-                s = source.getValue();
-               // update();
-                }
-            }
-	        
-            private void update(Mat img, Mat filteredImg) {
-                imgCaptureLabel.setIcon(new ImageIcon(HighGui.toBufferedImage(imgCapture)));
-                imgDetectionLabel.setIcon(new ImageIcon(HighGui.toBufferedImage(imgThresh)));
-                frame.repaint();
-            }
-    
-}
+		// Imgproc.resize(img, img, new Size(img.rows() / 2, img.rows() / 2), 0, 0,
+		// Imgproc.INTER_AREA);
+
+		// Use the content pane's default BorderLayout. No need for
+		// setLayout(new BorderLayout());
+		// Display the window
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	private void addComponentsToPane(Container pane, Image image) {
+		if (!(pane.getLayout() instanceof BorderLayout)) {
+			pane.add(new JLabel("Container doesn't use BorderLayout!"));
+			return;
+		}
+		sliderPanel = new JPanel();
+		saveButton = new JButton("Save");
+		imgLabel = new JLabel(new ImageIcon());
+		sliderS = new JSlider(0, BETA_SLIDER_MAX, 0);
+		sliderR = new JSlider(0, ALPHA_SLIDER_MAX, 0);
+
+		sliderPanel.setLayout(new BoxLayout(sliderPanel, BoxLayout.PAGE_AXIS));
+
+		sliderR.setMajorTickSpacing(20);
+		sliderR.setMinorTickSpacing(5);
+		sliderR.setPaintTicks(true);
+		sliderR.setPaintLabels(true);
+
+		sliderS.setMajorTickSpacing(20);
+		sliderS.setMinorTickSpacing(5);
+		sliderS.setPaintTicks(true);
+		sliderS.setPaintLabels(true);
+
+		// saveButton.setFocusable(true);
+		saveButton.addActionListener(this);
+
+		sliderS.addChangeListener(this);
+		sliderR.addChangeListener(this);
+
+		sliderPanel.add(new JLabel(String.format("Alpha x %d", ALPHA_SLIDER_MAX)));
+		sliderPanel.add(new JLabel(String.format("Beta x %d", BETA_SLIDER_MAX)));
+		sliderPanel.add(sliderS);
+		sliderPanel.add(sliderR);
+		pane.add(sliderPanel, BorderLayout.PAGE_START);
+		pane.add(imgLabel, BorderLayout.CENTER);
+	}
+
+	/**
+	 * Saves the image
+	 * 
+	 * @param e
+	 */
+	public void actionPerformed(ActionEvent e) {
+		save(img);
+	}
+
+	/**
+	 * Changes the image as the slider changes
+	 * 
+	 * @param e
+	 */
+	public void stateChanged(ChangeEvent e) {
+		if (e.getSource().equals(sliderS)) {
+			JSlider source = (JSlider) e.getSource();
+			r = source.getValue();
+			Mat red = redFilter(img, s);
+			haloFilter(red, r);
+			update(img);
+		}
+		if (e.getSource().equals(sliderR)) {
+			JSlider source = (JSlider) e.getSource();
+			s = source.getValue();
+			Mat red = redFilter(img, s);
+			haloFilter(red, r);
+			update(img);
+		}
+	}
+
+	/**
+	 * Updates the image when the slider changes value
+	 * 
+	 * @param filteredImg
+	 */
+	private void update(Mat filteredImg) {
+		imgLabel.setIcon(new ImageIcon(HighGui.toBufferedImage(filteredImg)));
+		frame.repaint();
+	}
 
 }
